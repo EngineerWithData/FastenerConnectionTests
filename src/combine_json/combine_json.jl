86bf794313be1124119b55dd7e8ce47d76bd78ec
Baihan Chen <baihan.chen@runtosolve.com>
2022-07-29 12:22:42 -0400

    add Zhidong_Zhang_2020 and combine_json

---
+++ src/combine_json/combine_json.jl
@@ -0,0 +1,13 @@
+using JSON3, JSON, DataFrames
+
+tao_data = read("src/Tao_et_al_2016/Tao_et_al_2016.json", String)
+
+cfs_data = read("src/Zhidong_Zhang_2020/cfs_fastener_test_data.json", String)
+
+line_break = """\n"""
+
+combined_data = string(tao_data, line_break, cfs_data)
+
+open("src/combine_json/combined_data.json", "w") do f
+    write(f, combined_data)
+end
